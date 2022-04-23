da59e1f0d3ac6f45dabb872e7fc3fb1158ff77be
naihuihe <103006952+naihuihe@users.noreply.github.com>
2022-04-24 00:23:45 +0100

    Delete line_seperator.py

--- configGUI_V1/widgets/line_seperator.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Frame
-from static.css import MainGuiStyle
-
-
-class LineSeperator(Frame):
-
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-        self.config(cnf = MainGuiStyle.line_seperator)
