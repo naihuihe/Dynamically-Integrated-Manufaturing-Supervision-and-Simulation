e4477cd9b5030f6f21492ca35349b7f0527fad77
naihuihe <103006952+naihuihe@users.noreply.github.com>
2022-04-24 00:31:13 +0100

    Update __init__.py

--- configGUI_V1/__init__.py
+++ configGUI_V1/__init__.py
@@ -4,5 +4,5 @@ PROJECT_BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 from configGUI_V1.main.main_window import MainWindow
 
 def create_main():
-    main_gui = MainWindow()
-    return main_gui
+    main = MainWindow()
+    return main
