f87bf17f745e9ae0124e7da119f07a81d2d87424
naihuihe <103006952+naihuihe@users.noreply.github.com>
2022-05-01 02:14:21 +0100

    Add files via upload

---
+++ configGUI_V1/agent_models/resource.py
@@ -0,0 +1,19 @@
+from agent_models.agent import Agent
+
+
+class Resource(Agent):
+
+
+    def __init__(self, wm, pm, parent, **kwargs):
+
+        super().__init__(wm, pm, parent, **kwargs)
+
+
+
+
+    def create_surface(self, window):
+        pass
+
+    def create_config_panel(self):
+
+        pass
